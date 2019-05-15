1a130ab1fe7c2aa5e93ed62c5f2a5aa7a5d5ee07
brianryu <s.brianryu@gmail.com>
2019-05-15 17:31:16 -0400

    All Routes Resolved, All Model Associations Fixed

--- app/controllers/api/v1/parts_controller.rb
+++ app/controllers/api/v1/parts_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PartsController < ApplicationController
+    before_action :find_part, only: [:update]
+
     def index
         @parts = Part.all 
         render json: @parts
@@ -12,6 +14,15 @@ class Api::V1::PartsController < ApplicationController
         @part = Part.create(part_params)
         render json: @part, status: :accepted
     end 
+
+    def update
+        @part.update(part_params)
+        if @part.save
+        render json: @part, status: :accepted
+        else
+        render json: { errors: @part.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
 
     private
     
