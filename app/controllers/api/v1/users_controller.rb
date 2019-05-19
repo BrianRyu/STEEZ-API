b620930ec9f9075fb0e0625094730a3809e8d28d
brianryu <s.brianryu@gmail.com>
2019-05-19 19:27:28 -0400

    user auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,20 @@ class Api::V1::UsersController < ApplicationController
         render json: { error: 'failed to create user' }, status: :not_acceptable
       end
     end
+
+    def show
+      @user = User.find(params[:id])
+      render json: @user
+    end
+
+    def login
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        render json: {user: UserSerializer.new(@user)}
+      else 
+        render json: {error: "Please Try Again"}
+      end
+    end 
    
     private
     def user_params
