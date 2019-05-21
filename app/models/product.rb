5e2b4709aefb344dc50ac760f71ef9fd245ee1d9
brianryu <s.brianryu@gmail.com>
2019-05-21 12:11:09 -0400

    Currently working on Delete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
     has_many :parts
     has_many :posts, through: :parts
+    validates :name, presence: true
+    validates :img_url, presence: true
+    validates :description, presence: true
 end
